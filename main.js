8161da8f59aa5fe9d4d70dedc84b315b2f595dd5
Alejandro Vasquez <josejaguer12@gmail.com>
2022-06-27 22:49:06 -0600

    Se avanzo con el llamado de la api solo falta ordenar y llamar a más pokemon

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
-import './style.css'
+const api = axios.create({
+  baseURL: "https://pokeapi.co/api/v2/",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+});
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+//Esta funcion fue creada solo para
+//llamarla y crear pokemon dentro de la
+//pokedex
+function createPokemon(pokemon) {
+  const pokeId = document.createElement("p");
+  pokeId.className = "pokemon-id";
+  pokeId.textContent = pokemon.id;
+
+  const pokeImage = document.createElement("img");
+  pokeImage.setAttribute("src", pokemon.sprites.front_default);
+  pokeImage.setAttribute("loading", "lazy");
+
+  const pokeText = document.createElement("div");
+  pokeText.className = "poke-text";
+
+  const pokeName = document.createElement("h4");
+  pokeName.className = "pokemon-name";
+  pokeName.innerText = pokemon.name;
+  pokeText.appendChild(pokeName);
+
+  const pokemonDiv = document.createElement("div");
+  pokemonDiv.className = "pokemon";
+  pokemonDiv.addEventListener("click", () => {
+    console.log(pokemon.name);
+  });
+
+  pokemonDiv.appendChild(pokeId);
+  pokemonDiv.appendChild(pokeImage);
+  pokemonDiv.appendChild(pokeText);
+
+  const pokemoMainDiv = document.querySelector(".pokedex-main");
+  pokemoMainDiv.appendChild(pokemonDiv);
+
+  const pokeContainer = document.querySelector(".poke-container");
+  pokeContainer.appendChild(pokemoMainDiv);
+}
+
+async function fetchPokemon() {
+  for (let poke = 1; poke <= 151; poke++) {
+    const { data } = await api(`pokemon/${poke}`);
+
+    createPokemon(data);
+  }
+}
+
+fetchPokemon();
