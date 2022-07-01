fe186c36321a2892d69006f3b084fbb017dfa716
Alejandro Vasquez <josejaguer12@gmail.com>
2022-07-01 11:48:58 -0600

    Se hicieron cambios

--- src/main.js
+++ src/main.js
@@ -1,16 +1,31 @@
+//Eventos del navegador
 window.addEventListener("DOMContentLoaded", navigator, false);
 window.addEventListener("hashchange", navigator, false);
 
+//Variables del DOM
 const menuBtn = document.querySelector(".pokedex-header i");
 const pokemonTypesContainer = document.querySelector(".pokemon--types");
 const pokemonMainDiv = document.querySelector(".pokedex-main");
+const pokedexHeader = document.querySelector(".pokedex-header--div");
 
+//Consulta a la api
 const api = axios.create({
   baseURL: "https://pokeapi.co/api/v2/",
   headers: {
     "Content-Type": "application/json;charset=utf-8",
   },
 });
+
+//Eventos addEventListener
+menuBtn.addEventListener("click", () => {
+  pokemonTypesContainer.classList.toggle("invisible");
+});
+
+pokedexHeader.addEventListener("click", () => {
+  location.hash = "";
+});
+
+//Funciones
 
 //Funcion para devolver la primera letra de una
 //palabra en mayuscla
@@ -27,7 +42,6 @@ function createPokemon(pokemon) {
 
   const pokeImage = document.createElement("img");
   pokeImage.setAttribute("src", pokemon.sprites.front_default);
-  pokeImage.setAttribute("loading", "lazy");
 
   const pokeText = document.createElement("div");
   pokeText.className = "poke-text";
@@ -57,6 +71,29 @@ function createPokemon(pokemon) {
 
   pokemonMainDiv.appendChild(pokemonDiv);
 }
+
+//Funcion para navegar a travez del navegador
+//por medio del location.hash
+function navigator() {
+  if (location.hash.startsWith("#type=")) {
+    typePage();
+  } else {
+    homePage();
+  }
+}
+
+function homePage() {
+  fetchPokemon();
+}
+
+function typePage() {
+  console.log("Estamos con un tipo de pokemon");
+  const [_, pokemonType] = location.hash.split("=");
+  pokemonTypesContainer.classList.add("invisible");
+  filterTypePokemon(pokemonType);
+}
+
+//Funciones asincronas
 
 async function fetchTypes() {
   const { data } = await api("type");
@@ -98,28 +135,6 @@ async function filterTypePokemon(type) {
       }
     });
   }
-}
-
-menuBtn.addEventListener("click", () => {
-  pokemonTypesContainer.classList.toggle("invisible");
-});
-
-function navigator() {
-  if (location.hash.startsWith("#type=")) {
-    typePage();
-  } else {
-    homePage();
-  }
-}
-
-function homePage() {
-  fetchPokemon();
-}
-
-function typePage() {
-  console.log("Estamos con un tipo de pokemon");
-  const [_, pokemonType] = location.hash.split("=");
-  filterTypePokemon(pokemonType);
 }
 
 fetchTypes();
